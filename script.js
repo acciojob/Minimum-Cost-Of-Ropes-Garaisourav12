212e563c33ad862ac4fb46867f25705bd5a35710
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-20 14:05:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	let ans = 0;
+	while(arr.length>1){
+		arr.sort((a, b) => a-b);
+		let add = (arr.shift() + arr.shift());
+		arr.unshift(add);
+		ans+=add;
+	}
+	return ans;
 }  
